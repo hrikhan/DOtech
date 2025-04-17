e4487072391f5a265fd0437f35ee3abcd96d08cf
hridoykhan51 <hridoykhan6786@gmail.com>
2025-04-17 17:41:44 +0600

    project sturcture done

---
+++ lib/screen/add_to_cart.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AddToCart extends StatelessWidget {
+  const AddToCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add to Cart'),
+      ),
+      
+    );
+  }
+}
